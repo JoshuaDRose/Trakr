84c4ba44750226d1d3d6b491e02af9ee63488f90
Joshua Rose <joshuarose099@gmail.com>
2023-03-02 16:17:28 +1100

    ✅ test(add): net test for install process

--- tests/unit/test_install.py
+++ tests/unit/test_install.py
@@ -2,9 +2,13 @@
 
 import src.install
 import unittest
-
+import requests
 
 class TestLoadDependencies(unittest.TestCase):
+
     def test_datatype(self):
         self.assertIsInstance(src.install(), dict)
 
+    def test_network(self):
+        response = requests.get('https://api.github.com/')
+        self.assertEquals(response.status_code, 200)
