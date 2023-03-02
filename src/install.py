63bbb9ca9c0ee58bb904c1ece1e4ff3282e03a02
Joshua Rose <joshuarose099@gmail.com>
2023-03-02 14:46:37 +1100

    💩stubs: added log stub; added dependency indexing  - Installation file now broken - see previous commit(s) for a working installation  - Stub currently worked on is talked about extensively in #6  - will fix broken install asap

--- src/install.py
+++ src/install.py
@@ -1,5 +1,6 @@
 """
 TODO: 🪴 emoticons
+TODO:    log folder
 """
 import sys
 import os
@@ -17,16 +18,26 @@ def load_dependencies(root) -> dict:
     dependency_file: dict = json.load(open(os.path.join(root, 'package.json'), 'r'))
     return dependency_file
 
-def locate_logger_config() -> str:
+def locate_directory(file = 'config.ini') -> str:
     """
     Description: Locate file through determining the root folder
 
     :return: str
     """
-    folder, file = os.path.split(os.path.join(os.path.split(os.path.dirname(os.getcwd()))[1], 'config.ini'))
+    folder, _file = os.path.split(os.path.join(os.path.split(os.path.dirname(os.getcwd()))[1], file))
     if folder.__ne__('Trakr'):
-        return os.path.join('src', file)
-    return file
+        return os.path.join('src', _file)
+    return _file
+
+def log_change(change: str, emoji: str = str()) -> bool:
+    """
+    :change: message of the change to be recorded
+    :emoji: unicode emoji character - NF* allowed
+
+    :return: If the function succeeded; interpreted as <true | false>
+    """
+    if 
+    with open(os.path.join(
 
 def main():
     """
@@ -41,13 +52,24 @@ def main():
     logger.info(" 🧰 Attempting to install packages")
     dependencies: dict = load_dependencies('' if os.path.split(os.path.dirname(os.getcwd()))[1].__ne__('Trakr') else '..')
 
+    # NOTE Dependencies that were updated in the process of installation
+    updated_dependencies: list[str] | list[None] = []
+
     for dependency in dependencies:
         if not dependencies[dependency]["installed"]:
             os.system('python -m pip install {dependency}=={version}'.format(
-            dependency=dependency,
-            version=dependencies[dependency]["version"]))
+                dependency=dependency,
+                version=dependencies[dependency]["version"]))
+
+    if len(updated_dependencies):
+        changed_dependencies = dependencies
+        for index, dependency in enumerate(updated_dependencies):
+            logger.info("🖊️ Updating dependency #{dependency_id}".format(
+                dependency_id=str(index)))
 
     logger.info(" 🎉 Success!")
 
 if __name__ == "__main__":
     main()
+
+# * NF = nerd font
