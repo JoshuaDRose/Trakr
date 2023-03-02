91a507e7277a71817a04cc78e6df322b0597d15d
Joshua Rose <joshuarose099@gmail.com>
2023-03-02 19:32:14 +1100

    💻 refactor(format): fit code to colorcolumn

--- src/install.py
+++ src/install.py
@@ -43,7 +43,7 @@ def handle_cli_args():
     Description: A file rarely needs change, but this
     allows developers the authority to do so in that case.
 
-    :return:
+    return: ...
     """
 
     @staticmethod
@@ -53,7 +53,7 @@ def handle_cli_args():
         complexity when reading the file. I can look at handle_cli_args
         as one function; it need not be so complex.
 
-        :return:
+        return: ...
         """
         package_file = locate_directory(file="package.json")
         logger.debug(f" 📥 Writing a reset to {package_file}")
@@ -82,12 +82,12 @@ def load_dependencies(root) -> dict:
 
     return: <dict>
     """
-    dependency_file_name: str = locate_directory(file="package.json")
+    dependency_file_name: str = locate_directory(
+            locate_dependencies(file="package.json"))
     try:
         load_dependency_as_object(dependency_file_name)
     except FileNotFoundError:
         logger.warning(" 🔎 Could not find file - changing location")
-        if 
         load_dependency_as_object(dependency_file_name)
     return dependency_file
 
@@ -101,14 +101,6 @@ def count_dependencies(file: dict) -> int:
     dependency_amount = len(file.keys())
     return dependency_amount
 
-def get_search_path() -> tuple:
-    """
-    Description: Used as a tool in io functions
-
-    return: <tuple>
-    """
-    folder, _file = os.path.split(os.path.join(os.path.split(os.path.dirname(os.getcwd()))[1], file))
-    return folder, file
 
 def locate_directory(file = "config.ini") -> str:
     """
@@ -116,10 +108,48 @@ def locate_directory(file = "config.ini") -> str:
 
     return: <str>
     """
-    folder, _file = os.path.split(os.path.join(os.path.split(os.path.dirname(os.getcwd()))[1], file))
+    def get_search_path() -> tuple:
+        """
+        Description: Used as a tool in io functions
+
+        return: <tuple>
+        """
+        nonlocal folder, file
+        folder, file = os.path.split(os.path.join(
+            os.path.split(os.path.dirname(os.getcwd()))[1], file))
+        return folder, file
+    folder, _file = 
     if folder.__ne__("Trakr"):
         return os.path.join("src", _file)
     return _file
+
+def query_create_log_file():
+    """
+    Description: Queries user as to if they'd like to create
+    a logfile in the case that a file is not present.
+
+    return: ...
+    """
+    logger.info(""" 🔎 Could not find {file}.
+   Would you like to create a log file? [n/Y] """)
+    _input = input(logger.debug())
+    try:
+        match _input.lower():
+            case "" | "yes" | "y":
+                with open(file, "w") as fp: fp.close()
+                logger.debug(""" ✅ Created {} in {} """.format(
+                file, os.path.join(os.getcwd(), file)))
+                log_change(change, _file, emoji)
+                return
+            case "n":
+                logger.debug("   Ignoring log file creation.")
+                return
+            case _:
+                return
+    except Exception as error:
+        logger.critical(error)
+        sys.exit(error.errno)
+        
 
 def log_change(change: str, _file="install.log", emoji: str = str()) -> bool:
     """
@@ -129,74 +159,63 @@ def log_change(change: str, _file="install.log", emoji: str = str()) -> bool:
     return: <true | false>
     """
     file = locate_directory(_file)
-    # logger.debug("🥾 Removed un-needed variable: {file_content}", file_content=_file.__repr__())
     fp = object()
     try:
         fp = open(file, "a")
     except FileNotFoundError:
-        _input = input(logger.debug(f" 🔎 Could not find {file}. Would you like to create a log file? [n/Y] >> "))
-        try:
-            match _input.lower():
-                case "" | "yes" | "y":
-                    with open(file, "w") as fp: fp.close()
-                    logger.debug(f" ✅ Created {file} in {os.path.join(os.getcwd(), file)} ")
-                    log_change(change, _file, emoji)
-                    return
-                case "n":
-                    logger.debug("   Ignoring log file creation.")
-                    return
-                case _:
-                    return
-        except Exception as error:
-            logger.critical(error)
-            sys.exit(error.errno)
 
 def create_file(filename):
     """
-    filename <str> the name of the file... Every 60 seconds in Africa, a minute passes.
+    filename <str> the name of the file...
+    Every 60 seconds in Africa, a minute passes.
 
-    :return:
+    return: ...
     """
     logger.info(f" 📑 Creating {filename} in {os.getcwd()}")
     try:
         with open(filename, "w", False, "utf-8") as file:
             file.close()
     except PermissionError as error:
-        logger.critical(f" ❌ Could not create {filename}: insufficient persmissions")
+        logger.critical(
+        f""" ❌ Could not create {filename}: insufficient persmissions.""")
         sys.exit(error.errno)
 
 def main():
     """
     Description: Entry point for installation
 
-    :return:
+    return: ...
     """
     logger.info(" 🧰 Attempting to install packages")
-    dependencies: dict = load_dependencies("" if os.path.split(os.path.dirname(os.getcwd()))[1].__ne__("Trakr") else "..")
+    dependencies: dict = load_dependencies("package.json")
     # NOTE Dependencies that were updated in the process of installation
     updated_dependencies = {}
 
     for dependency in dependencies:
         if not dependencies[dependency]["installed"]:
-            # https://stackoverflow.com/questions/3503879/assign-output-of-os-system-to-a-variable-and-prevent-it-from-being-displayed-on
             # NOTE Returns an open file object connected to pipe 
             os.popen("python -m pip install {dependency}=={version}".format(
                 dependency=dependency,
-                version=dependencies[dependency]["version"])).read()  # NOTE read suppresses output
+                # NOTE read suppresses output
+                version=dependencies[dependency]["version"])).read()
             # Assign an "is_installed" | "installed" value.
             updated_dependencies[dependency] = {}
-            updated_dependencies[dependency]["installed"] = dependencies[dependency]["installed"]
+            updated_dependencies[dependency]["installed"] = \
+                    dependencies[dependency]["installed"]
             updated_dependencies[dependency] = dependencies[dependency]
 
     if len(updated_dependencies.keys()):
         changed_dependencies = dependencies
-        dependency_count = count_dependencies(changed_dependencies)  # NOTE when adding test cases, test both vars equal
+        # NOTE when adding test cases, test both vars equal
+        dependency_count = count_dependencies(changed_dependencies)
         for index, dependency in enumerate(updated_dependencies):
-            logger.info(" [{dependency_id}/{dependency_amt}] Updating dependency {dependency_name}={dependency_version}".format(
+            logger.info(""" [{dependency_id}/{dependency_amt}] Updating
+dependency {dependency_name}={dependency_version}""".format(
                 dependency_id=str(index),
                 dependency_amt=dependency_count,
                 dependency_name=dependency,
-                dependency_version=updated_dependencies[dependency]["version"]))
+                dependency_version=\
+                        updated_dependencies[dependency]["version"]))
 
     logger.info(" 🎉 Success!")
 
